2fb132271722e16288b9b2c6015459a03dcea616
= <=>
2024-02-11 00:26:13 -0300

    11/02/24 0:26 Hs

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,27 @@
+import { connect } from '@/dbConfig/dbConfig'
+import User from '@/models/userModel' 
+import { NextRequest, NextResponse } from 'next/server'
+import bcryptjs from 'bcryptjs'
+
+connect()
+
+export async function GET( request : NextRequest){
+    try {
+        const reqBody = await request.json()
+        const {email, password} = reqBody
+        console.log(reqBody)
+
+        // check if user exits
+
+        const user = await User.findOne({email})
+
+        if(!user){
+            return NextResponse.json({error: "User dos not exist"}, {status: 400})
+        }
+
+
+        
+    } catch (error : any) {
+        return NextResponse.json({error: error.message})
+    }
+}
