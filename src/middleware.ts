449df0c895d6c8926c2a087e57b8147b77de82c3
= <=>
2024-02-14 22:13:42 -0300

    middleware 14/2/24 22:1 3 Hs

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,20 @@ import { NextRequest, NextResponse } from "next/server";
 // This function can be marked 'async' if using 'await' inside
 
 export function middleware( request: NextRequest){
-    const path = request.nextUrl.pathname
+    // The token cant' see for others users
+    const path = request.nextUrl.pathname // a Public Path
 
-    const isPublicPath = path === '/login' || path === '/signup'
+    const isPublicPath = path === '/login' || path === '/signup' // this two path , public , in this path i can't see the token
 
-    const token = request.cookies.get('token')?.value || ''
+    const token = request.cookies.get('token')?.value || '' // Si esta el token - lo elimina
 
-    /* if(isPublicPath && token) {
-        return NextResponse.redirect()
-    } */
+    if(isPublicPath && token) {
+        return NextResponse.redirect(new URL("/", request.nextUrl)) // redirecciona
+    }
+    
+    if(!isPublicPath && !token) {
+        return NextResponse.redirect(new URL('/login', request.nextUrl))
+    }
 }
 
 // See "Matching Paths" below to learn more
