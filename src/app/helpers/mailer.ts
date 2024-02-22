0c99be858e0c5b9978cc5d3202c547efc8d2448c
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-02-22 18:06:10 -0300

    2/22/24 6:06PM

--- src/app/helpers/mailer.ts
+++ src/app/helpers/mailer.ts
@@ -5,7 +5,18 @@ import bcryptjs from "bcryptjs"
 export const sendEmail = async({email, emailType, userId} : any) => {
     try{
 
+        // create hashToken
+        const hashToken = bcryptjs.hash(userId.toString(), 10)
+
+        await User.findByIdAndUpdate(userId,
+             {
+            verifyToken: hashToken,
+            verifyTokenExpiry: Date.now() + 3600000
+             }/* ,
+             {
+            new: true,  runValidators: true
+             } */)
     } catch (error : any){
-        
+        throw new Error(error.message)
     }
 }
